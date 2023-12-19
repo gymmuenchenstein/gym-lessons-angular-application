7e242aca0cda61a0c75030b69d5efaf878ea28a8
matttheman <passing.canoe.05@icloud.com>
2023-12-19 17:50:43 +0100

    created accordion for menu

--- src/app/view/components/menu/menu.component.ts
+++ src/app/view/components/menu/menu.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {
+    NgbAccordionBody,
+    NgbAccordionButton, NgbAccordionCollapse,
+    NgbAccordionDirective,
+    NgbAccordionHeader,
+    NgbAccordionItem, NgbModule
+} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+    imports: [
+        NgbModule
+    ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
