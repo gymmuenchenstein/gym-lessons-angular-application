acf879d070e2d0fb4f497a071e92003db73f49f9
e236119 <serious-theo@seriouscircus.com>
2023-12-19 17:58:45 +0100

    Played around with timetable

--- src/app/view/components/timetable/timetable.component.ts
+++ src/app/view/components/timetable/timetable.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {TimetableDayComponent} from "./timetable-day/timetable-day.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-timetable',
   standalone: true,
-  imports: [],
+    imports: [
+        TimetableDayComponent,
+        NgForOf
+    ],
   templateUrl: './timetable.component.html',
   styleUrl: './timetable.component.scss'
 })
 export class TimetableComponent {
+
+    Montag=[["M","lession M _1"]]
+    Dienstag=[["M","lession M _1"],["D","lession M _2"],["D","lession M _2"]]
+
+    entries: any[][] =[[this.Montag,"Montag"], [this.Dienstag,"Dienstag"]]
 
 }
