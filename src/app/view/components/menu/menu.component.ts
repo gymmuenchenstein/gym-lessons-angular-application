ebff807789c7a9977ae2daa826d258a6ddd477ea
Andreas Aeschlimann <andreas.aeschlimann@appvision.ch>
2024-01-17 00:39:43 +0100

    feat: apply data to the accordion structure

--- src/app/view/components/menu/menu.component.ts
+++ src/app/view/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {
     NgbAccordionBody,
     NgbAccordionButton, NgbAccordionCollapse,
@@ -7,8 +7,10 @@ import {
     NgbAccordionItem, NgbModule
 } from "@ng-bootstrap/ng-bootstrap";
 import {FormsModule} from "@angular/forms";
-import {JsonPipe} from "@angular/common";
+import {CommonModule, JsonPipe, NgForOf} from "@angular/common";
 import {debounceTime, distinctUntilChanged, map, Observable, OperatorFunction} from "rxjs";
+import {CalendarDataBrokerService} from "../../../services/calendar-data/calendar-data-broker.service";
+import {AccordionComponent, AccordionData} from "../accordion/accordion.component";
 
 const states = [
     'Alabama',
@@ -74,16 +76,101 @@ const states = [
 
 
 @Component({
-  selector: 'app-menu',
-  standalone: true,
+    selector: 'app-menu',
+    standalone: true,
     imports: [
-        NgbModule, FormsModule, JsonPipe
+        NgbModule, FormsModule, JsonPipe, CommonModule, AccordionComponent
     ],
-  templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+    templateUrl: './menu.component.html',
+    styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
     model: any;
+
+    classData: AccordionData = {
+        label: "Klassen",
+        nestedData: [{
+            label: "Maturitätsklassen",
+            nestedData: [],
+        }, {
+            label: "Fachmaturitätsklassen",
+            nestedData: [],
+        }, {
+            label: "Andere",
+            nestedData: [],
+        }]
+    };
+
+    teacherData: AccordionData = {
+        label: "Lehrpersonen",
+        nestedData: []
+    };
+
+    roomData: AccordionData = {
+        label: "Räume",
+        nestedData: []
+    };
+
+    constructor(private broker: CalendarDataBrokerService) {
+
+        this.broker.onInitialized.subscribe(() => {
+
+            // Get all classes
+            this.getClassData();
+
+            // Get all teachers
+            this.getTeacherData();
+
+            // Get all rooms
+            this.getRoomData();
+
+        });
+
+    }
+
+    private getClassData(): void {
+
+        const allClassesData = this.broker.unique().classes.filter(c => c).sort();
+        const allNestedData = allClassesData.map((s) => {
+            return {
+                label: s,
+                action: () => this.openTimetable(s),
+            }
+        });
+
+        this.classData.nestedData![0].nestedData = allNestedData.filter((s) => s.label.startsWith("M"));
+        this.classData.nestedData![1].nestedData = allNestedData.filter((s) => s.label.startsWith("F"));
+        this.classData.nestedData![2].nestedData = allNestedData.filter((s) => !s.label.startsWith("M") && !s.label.startsWith("F"));
+
+    }
+
+    private getTeacherData(): void {
+
+        const teachers = this.broker.unique().teachers;
+        this.teacherData.nestedData = teachers.map((teacher) => {
+            return {
+                label: teacher.surname + ", " + teacher.name + " (" + teacher.abbr + ")",
+                action: () => this.openTimetable(teacher),
+            }
+        });
+
+    }
+
+    private getRoomData(): void {
+
+        const rooms = this.broker.unique().rooms.filter(r => r).sort();
+        this.roomData.nestedData = rooms.map((room) => {
+            return {
+                label: room,
+                action: () => this.openTimetable(room),
+            }
+        });
+
+    }
+
+    private openTimetable(data: any): void {
+        console.log(data);
+    }
 
     formatter = (result: string) => result.toUpperCase();
 
