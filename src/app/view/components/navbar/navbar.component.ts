ddb340696e485d2b3e9bf540d46ea14bad7d3db8
Andreas Aeschlimann <andreas.aeschlimann@appvision.ch>
2024-02-06 00:43:53 +0100

    feat: group page into sections

--- src/app/view/components/navbar/navbar.component.ts
+++ src/app/view/components/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MenuService} from "../../../model/services/menu.service";
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+    constructor(protected menuService: MenuService) {
+    }
 
 }
