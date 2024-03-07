4446e123fca675266642787558d67153fa3a15f7
W <redstony@walmail.ch>
2024-03-07 04:48:42 +0100

    fixed issue #49

--- src/app/view/components/details/details.component.ts
+++ src/app/view/components/details/details.component.ts
@@ -36,7 +36,7 @@ export class DetailsComponent implements AfterViewInit {
         this.entry = this.broker.getSelectEntry();
         this.time = this.entry?.datetime?.hour() + ":" + this.entry?.datetime?.minute() + " - " + this.entry?.datetime?.add(this.entry?.duration != undefined ? this.entry?.duration : { minutes: 45 }).hour() + ":" + this.entry?.datetime?.add(this.entry?.duration != undefined ? this.entry?.duration : { minutes: 45 }).minute();
         this.teachers = "" + this.entry?.teachers.map(el => {
-            return el.surname + " " + el.name
+            return el.surname + " " + el.name + " (" + el.abbr + ")";
         }).join(", ");
         this.classes = "" + this.entry?.classes.map(el => {
             return el.full;
